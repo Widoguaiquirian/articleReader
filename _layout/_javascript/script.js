5c60ab80f460546858cbc349d921ddf05bdb228b
Luis Guaiquirian <luisdavidguaiquirian@gmail.com>
2022-08-04 14:39:10 -0400

    Adding pause button

--- _layout/_javascript/script.js
+++ _layout/_javascript/script.js
@@ -4,12 +4,22 @@
 const sound = document.querySelector('.sound-1');
 const btnPlay = document.querySelector('.btnPlay');
 const btnStop = document.querySelector('.btnStop');
+let count = 0;
 
 btnPlay.addEventListener('click', function () {
-  sound.play();
+  if (count === 0) {
+    btnPlay.innerHTML = `<i class="fa fa-pause"></i> Detener lectura`;
+    count = 1;
+    sound.play();
+  } else {
+    count = 0;
+    sound.pause();
+    btnPlay.innerHTML = `<i class="fas fa-play"> </i> Escuchar artículo`;
+  }
 });
 
 btnStop.addEventListener('click', function () {
   sound.pause();
   sound.currentTime = 0;
+  btnPlay.innerHTML = `<i class="fas fa-play"> </i> Escuchar artículo`;
 });
